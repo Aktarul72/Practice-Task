5284572c533040ee47663bc581bb6c0a8015e938
Md. Aktarul Islam <60525055+Sony72@users.noreply.github.com>
2022-04-10 22:10:20 +0600

    first commit

---
+++ Lomsh/view/Loms.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="icon" href="../model/image/LOGO1.png"/>
+    <link rel="stylesheet" href="../model/css/style.css"/>
+    <title>LOMS</title>
+</head>
+<body>
+
+
+<marquee width="100%" direction="left" height="50px">
+        Welcome to <b>Lab One Medical Services & Hospital</b> Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Itaque, doloribus! Alias inventore suscipit,
+        laudantium asperiores iste quisquam laboriosam praesentium rem!
+      </marquee>
+
+<div class="main-container">
+      <div class="landing-page__first-part">
+          <div class="landing-page_first-part_title">
+          <h1 style="text-align:center"><br><b>Lab One Medical Services & Hospital</b></h1>
+          <p class="landing-page__first-part__paragraph">
+            <b>Everyday or life-changing care. We're here when you need us.</b>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="landing-page__second-part-services">
+        <h2>Our Services</h2>
+        <ul>
+          <li>Doctor consultation</li>
+          <li>IPD</li>
+          <li>OPD</li>
+          <li>Proper diagonosis sytem</li>
+          <li>Pathologyp</li>
+          <li>Cardiology</li>
+          <li>Operation Theater</li>
+        </ul>
+      </div>
+
+
+        <iframe
+        width="100%"
+        height="500"
+        src="../model/video/Ad Film _ LAB ONE Medical Services & Hospital _ 24 Hours Pharmacy _ All Kind Operation.mp4"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+        ></iframe>
+
+    
+</body>
+</html>
